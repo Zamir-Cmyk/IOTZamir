3b5b9de62c4a0ab5959c1b410407bcdcc978d210
Zamir-Cmyk <widyzaidy97@gmail.com>
2025-09-19 11:36:10 +0800

    Create 15.Frekuensi Karakter.cpp

---
+++ LATIHAN/15.Frekuensi Karakter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+  string teks = "programming";
+  map<char, int> frekuensi;
+
+  for (char c : teks) {
+    frekuensi[c]++;
+  }
+
+  cout << "Frekuensi karakter: " << endl;
+  for (auto pair : frekuensi) {
+    cout << "'" << pair.first << "': " << pair.second << endl;
+  }
+  return 0;
+}
