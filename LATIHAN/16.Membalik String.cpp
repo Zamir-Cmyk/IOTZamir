095c8f4e506d2bafba3fca4270f1445f36467fcf
Zamir-Cmyk <widyzaidy97@gmail.com>
2025-09-19 11:42:29 +0800

    Create 16.Membalik String.cpp

---
+++ LATIHAN/16.Membalik String.cpp
@@ -0,0 +1,15 @@
+#include <iostring>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  string teks;
+
+  cout << "Masukkan sebuah string: ";
+  getline(cin, teks);
+  reverse(teks.begin());
+  cout << "Hasil string setelah dibalik: " << teks << endl;
+  return 0;
+}
